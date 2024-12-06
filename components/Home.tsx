d88d7a31dbfb64c493a0983b29e61bc97ce1c750
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2024-12-06 13:27:27 +0530

    fix:initial_ui_fix_1

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,52 +1,56 @@
 'use client'
-import { FaCheck } from "react-icons/fa6";
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
+import { VscDebugBreakpointData } from "react-icons/vsc";
 
 const Home = () => {
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    // `useInView` hook to check if the element is in view
+    const isInView = useInView(ref, { once: true });
+
   return (
-    <div className="flex flex-col w-full lg:flex-row justify-center items-center p-6 mt-6 bg-gradient-to-br hover:bg-gradient-to-r from-slate-900 dark:from-cyan-800 dark:to-slate-900 to-cyan-800  ">
-        <section className="flex flex-col justify-center items-start gap-6">
-            <div className="text-xl md:text-3xl lg:text-4xl text-white font-bold">
-                Craft Your Future with the  
-                <span className="text-cyan-600"> Perfect </span>
-                Resume
-            </div>
-            <section className="lg:hidden ">
-                <img src='/resume.png' alt="resume" className="h-52 w-52 p-2"></img>
-            </section>
-            <section className="flex  flex-col gap-2 items-start justify-start text-[0.7rem] md:text-[0.85rem]  font-normalhover:bg-gradient-to-r bg-gradient-to-l from-slate-800 via-gray-200 to-cyan-600 dark:to-white bg-clip-text text-transparent">
-                    <div className="gap-2 flex flex-row items-center justify-center ">
-                        <FaCheck className="text-green-700"/><div>Build the Resume That Opens Doors.</div>
-                    </div>
-                    <div className="gap-2 flex flex-row items-center justify-center ">
-                        <FaCheck className="text-green-700"/><div>Your Dream Job Starts Here—Create the Perfect Resume.</div>
-                    </div>
-                    <div className="gap-2 flex flex-row items-center justify-center ">
-                        <FaCheck className="text-green-700"/><div>Transform Your Skills into Opportunities with a Tailored Resume.</div>
-                    </div>
-                    <div className="gap-2 flex flex-row items-center justify-center">
-                        <FaCheck className="text-green-700"/><div>Get Noticed with a Resume That Speaks Volumes.</div>
-                    </div>
+    <motion.div
+    ref={ref}
+    initial={{ opacity: 0, y: 50 }} 
+    animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}  
+    transition={{ duration: 1.0, ease: 'easeOut' }}
+    className="">
+        <div className="flex flex-col w-full lg:flex-row justify-center items-center p-6 mt-4  ">
+            <section className="flex flex-col justify-center items-start gap-6">
+                <div className="text-xl md:text-3xl lg:text-4xl  font-bold">
+                    Craft Your Future with the  
+                    <span className="text-cyan-600"> Perfect </span>
+                    Resume
+                </div>
+                <section className="lg:hidden ">
+                    <img src='/resume.png' alt="resume" className="h-52 w-52 p-2"></img>
+                </section>
+                <section className="flex  flex-col gap-2 items-start justify-start text-[0.8rem] md:text-[0.85rem]  font-normal hover:bg-gradient-to-r bg-gradient-to-l from-slate-800  to-cyan-700 dark:from-cyan-400 dark:to-white bg-clip-text text-transparent">
+                        <div className="gap-2 flex flex-row items-center justify-center ">
+                            <VscDebugBreakpointData  className="text-cyan-700"/><div>Build the Resume That Opens Doors.</div>
+                        </div>
+                        <div className="gap-2 flex flex-row items-center justify-center ">
+                            <VscDebugBreakpointData  className="text-cyan-700"/><div>Your Dream Job Starts Here—Create the Perfect Resume.</div>
+                        </div>
+                        <div className="gap-2 flex flex-row items-center justify-center ">
+                            <VscDebugBreakpointData  className="text-cyan-700"/><div>Transform Your Skills into Opportunities with a Tailored Resume.</div>
+                        </div>
+                        <div className="gap-2 flex flex-row items-center justify-center">
+                            <VscDebugBreakpointData  className="text-cyan-700"/><div>Get Noticed with a Resume That Speaks Volumes.</div>
+                        </div>
 
+                </section>
             </section>
-        </section>
 
-        <section className="hidden lg:flex ">
-            <img src='/resume.png' alt="resume" className="h-80 w-96 p-2"></img>
-        </section>
-      
-    </div>
+            <section className="hidden lg:flex ">
+                <img src='/resume.png' alt="resume" className="h-80 w-96 p-2"></img>
+            </section>
+        
+        </div>
+    </motion.div>
   )
 }
 
 export default Home
-
-{/* "Craft Your Future with the Perfect Resume."
-This phrase emphasizes the app's role in helping users create a resume that sets them up for success. It has a motivational feel and suggests that the resume you build with the app can have a lasting impact on the user's career.
-
-If you want to try a few variations, here are some more ideas:
-
-"Build the Resume That Opens Doors."
-"Your Dream Job Starts Here—Create the Perfect Resume."
-"Transform Your Skills into Opportunities with a Tailored Resume."
-"Your Career Journey Begins with the Right Resume."
-"Get Noticed with a Resume That Speaks Volumes. */}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client"
+import { motion, useInView } from 'framer-motion';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { CgMenuGridO } from "react-icons/cg";
 import { IoClose } from "react-icons/io5";
 import { Button } from './ui/button';
@@ -12,30 +13,41 @@ const AppLogo = dynamic(() => import('@/components/AppLogo'))
 
 
 const Navbar = () => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    // `useInView` hook to check if the element is in view
+    const isInView = useInView(ref, { once: true });
     const [menu,setMenu] = useState<boolean>(false);
     return (
-        <div className='p-4 flex flex-row items-center w-full justify-between shadow-md dark:shadow-sm dark:shadow-cyan-600'>
-            <AppLogo/>
-            <div className='hidden md:flex flex-row gap-2 justify-between items-center'>
-                <ThemeToggler/>
-                <Link href='login'><Button>Login</Button></Link>
-            </div>
-            {/* for mobile screen */}
-            <div className='md:hidden'>
-                {!menu && <CgMenuGridO onClick={()=>setMenu((prev)=>!prev)} className='text-cyan-600 '/>}
-                {menu && <IoClose onClick={()=>setMenu((prev)=>!prev)} className='text-cyan-600' />}
-                {menu && 
-                    <section className='absolute top-14 right-2 p-2 gap-2 bg-white dark:bg-slate-900 shadow-sm shadow-cyan-600
-                                        flex flex-col justify-center items-center'>
+        <motion.div
+            ref={ref}
+            initial={{ opacity: 0, x: '100%' }}  // Start off-screen to the right
+            animate={{ opacity: isInView ? 1 : 0, x: isInView ? 0 : '100%' }}  // Slide in from the right when in view
+            transition={{ duration: 0.5, ease: 'easeIn' }}
+            className="w-full">
+                <div className='p-4 flex flex-row items-center w-full justify-between shadow-md dark:shadow-sm dark:shadow-cyan-600'>
+                    <AppLogo/>
+                    <div className='hidden md:flex flex-row gap-2 justify-between items-center'>
                         <ThemeToggler/>
                         <Link href='login'><Button>Login</Button></Link>
+                    </div>
+                    {/* for mobile screen */}
+                    <div className='md:hidden'>
+                        {!menu && <CgMenuGridO onClick={()=>setMenu((prev)=>!prev)} className='text-cyan-600 '/>}
+                        {menu && <IoClose onClick={()=>setMenu((prev)=>!prev)} className='text-cyan-600' />}
+                        {menu && 
+                            <section className='absolute top-14 right-2 p-2 gap-2 bg-white dark:bg-slate-900 shadow-sm shadow-cyan-600
+                                                flex flex-col justify-center items-center'>
+                                <ThemeToggler/>
+                                <Link href='login'><Button>Login</Button></Link>
 
-                    </section>
-                }
-                
+                            </section>
+                        }
+                        
 
-            </div>
-        </div>
+                    </div>
+                </div>
+        </motion.div>
     )
 }
 
